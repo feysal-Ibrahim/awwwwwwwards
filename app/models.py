f4676059b6ac07cce33ec32e3c6181529af83598
feysal-Ibrahim <addictivefazman@gmail.com>
2018-10-15 08:14:02 +0300

    create post model

--- app/models.py
+++ app/models.py
@@ -30,3 +30,44 @@ class Profile( models.Model ):
     def find_profile(cls , search_term):
         profile=cls.objects.filter( user__username__icontains=search_term )
         return profile
+class Post(models.Model):
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='images/', blank=True)
+    description = HTMLField(blank=True)
+    link=models.URLField(blank=True)
+    profile=models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    @classmethod
+    def search_by_name(cls,search_term):
+        posts = cls.objects.filter(name__icontains=search_term)
+        return posts
+
+    @classmethod
+    def one_post(cls, id):
+        post=Post.objects.filter(id=id)
+        return post
+
+    @classmethod
+    def all_posts(cls):
+        posts = cls.objects.all()
+        return posts
+
+    @classmethod
+    def get_user_posts(cls, profile_id):
+        images=Post.objects.filter(profile_id=id)
+
+    @classmethod
+    def get_profile_image(cls, profile):
+        posts = Post.objects.filter(user_profile__pk=profile)
+        return posts
+
+    @classmethod
+    def get_post_by_id(cls,id):
+        post = Post.objects.filter(id = Post.id)
+        return post
+
+    @classmethod
+    def get_all_profiles(cls):
+        profile = Profile.objects.all()
+        return profile
