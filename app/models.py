8e4dcf43434fffee60ad9cd604b55c6914029e75
feysal-Ibrahim <addictivefazman@gmail.com>
2018-10-15 08:22:27 +0300

    create first veiw function

--- app/models.py
+++ app/models.py
@@ -6,9 +6,55 @@ import datetime as dt
 
 # Create your models here.
 
+# user post model
+class Post( models.Model ):
+    name=models.CharField( max_length=30 )
+    image=models.ImageField( upload_to='images/' , blank=True )
+    description=HTMLField( blank=True )
+    link=models.URLField( blank=True )
+    profile=models.ForeignKey( Profile , on_delete=models.CASCADE , null=True )
+    user=models.ForeignKey( User , on_delete=models.CASCADE )
+    date=models.DateTimeField( auto_now_add=True )
+
+    @classmethod
+    def search_by_name(cls , search_term):
+        posts=cls.objects.filter( name__icontains=search_term )
+        return posts
+
+    @classmethod
+    def one_post(cls , id):
+        post=Post.objects.filter( id=id )
+        return post
+
+    @classmethod
+    def all_posts(cls):
+        posts=cls.objects.all( )
+        return posts
+
+    @classmethod
+    def get_user_posts(cls , profile_id):
+        images=Post.objects.filter( profile_id=id )
+
+    @classmethod
+    def get_profile_image(cls , profile):
+        posts=Post.objects.filter( user__pk=profile )
+        return posts
+
+    @classmethod
+    def get_post_by_id(cls , id):
+        post=Post.objects.filter( id=Post.id )
+        return post
+
+    @classmethod
+    def get_all_profiles(cls):
+        profile=Profile.objects.all( )
+        return profile
+
+
+# user profile model
 class Profile( models.Model ):
     profilePic=models.ImageField( upload_to='profile/' , null=True , blank=True )
-    contact = HTMLField()
+    contact=HTMLField( )
     bio=models.CharField( max_length=60 , blank=True )
     user=models.ForeignKey( User , on_delete=models.CASCADE )
 
@@ -29,45 +75,4 @@ class Profile( models.Model ):
     @classmethod
     def find_profile(cls , search_term):
         profile=cls.objects.filter( user__username__icontains=search_term )
-        return profile
-class Post(models.Model):
-    name = models.CharField(max_length=30)
-    image = models.ImageField(upload_to='images/', blank=True)
-    description = HTMLField(blank=True)
-    link=models.URLField(blank=True)
-    profile=models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
-    date = models.DateTimeField(auto_now_add=True)
-
-    @classmethod
-    def search_by_name(cls,search_term):
-        posts = cls.objects.filter(name__icontains=search_term)
-        return posts
-
-    @classmethod
-    def one_post(cls, id):
-        post=Post.objects.filter(id=id)
-        return post
-
-    @classmethod
-    def all_posts(cls):
-        posts = cls.objects.all()
-        return posts
-
-    @classmethod
-    def get_user_posts(cls, profile_id):
-        images=Post.objects.filter(profile_id=id)
-
-    @classmethod
-    def get_profile_image(cls, profile):
-        posts = Post.objects.filter(user_profile__pk=profile)
-        return posts
-
-    @classmethod
-    def get_post_by_id(cls,id):
-        post = Post.objects.filter(id = Post.id)
-        return post
-
-    @classmethod
-    def get_all_profiles(cls):
-        profile = Profile.objects.all()
         return profile
--- app/views.py
+++ app/views.py
@@ -4,11 +4,12 @@ from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render,redirect
 from django.contrib.auth import  authenticate,login
 from django.views.generic import View
+from .models import Post
 
 # Create your views here.
 class IndexView(generic.ListView):
-    template_name = 'music/index.html'
-    context_object_name = 'all_albums'
+    template_name = 'award/index.html'
+    context_object_name = 'all_post'
 
     def get_queryset(self):
-        return Album.objects.all()
+        return Post.objects.all()
