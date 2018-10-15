67c0d8866b2e13c2b57d63e1391b5b55eb654937
feysal-Ibrahim <addictivefazman@gmail.com>
2018-10-15 08:01:42 +0300

    create app

---
+++ app/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+urlpatterns=[
+    # /app/
+    url( r'^$' , views.IndexView.as_view( ) , name='index' ) ,
+]
+
+if settings.DEBUG:
+    urlpatterns += static( settings.MEDIA_URL , document_root=settings.MEDIA_ROOT )
